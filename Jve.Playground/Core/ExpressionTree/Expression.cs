a5deb675e9c9d4b1e4365bcd2223e27dff5836f0
Jan Vium Enghoff <jan.jve@gmail.com>
2016-11-06 10:34:41 +0100

    expression test

---
+++ Jve.Playground/Core/ExpressionTree/Expression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ExpressionTree
+{
+    public class Expression : Element
+    {
+        public Element Left { get; set; }
+        public Element Right { get; set; }
+        public Operator Operator { get; set; }
+    }
+}
---
+++ Jve.Playground/CoreTests/ExpressionTree/ExpressionTreeTests.cs
@@ -0,0 +1,25 @@
+﻿using Core.Alignment;
+using Core.ExpressionTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simple = Core.Alignment.StepAlignmentConfiguration;
+
+namespace CoreTests.AlignmentTests
+{
+    [TestClass]
+    public class ExpressionTreeTests
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var literal = new Literal { Value = "1" };
+            var literal2 = new Literal { Value = "2" };
+
+            var constraint = new Constraint();
+            var expression = new Expression();
+            expression.Left = literal;
+            expression.Right = literal2;
+            
+        }
+
+    }
+}
